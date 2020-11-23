0bfb2ad5072a82cb958f517d0e61dc32898f16e2
roydeanjr <36243458+roydeanjr@users.noreply.github.com>
2020-11-22 23:27:07 -0600

    Update cds.php
    Update to no nav and large font

--- views/_layouts/cds.php
+++ views/_layouts/cds.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    {!! get_metas() !!}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {!! get_favicon() !!}
+    <title>{{ sprintf(lang('admin::lang.site_title'), Template::getTitle(), setting('site_name')) }}</title>
+    {!! get_style_tags() !!}
+</head>
+<body class="page {{ $this->bodyClass }}">
+    <div class="page-wrapper">
+        <div class="page-content">
 <?php 
 	
 	[$locationParam] = $this->getParams();
@@ -60,3 +72,14 @@
 	    </div>
     </div>		    	    
 </div>
+        </div>
+    </div>
+
+    <div id="notification">
+        {!! $this->makePartial('flash') !!}
+    </div>
+    {!! $this->makePartial('set_status_form') !!}
+    {!! Assets::getJsVars() !!}
+    {!! get_script_tags() !!}
+</body>
+</html>
