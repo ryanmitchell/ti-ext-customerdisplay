dce74a6d53c8678d61759d228045c7331f4b1264
Ryan Mitchell <ryan@thoughtcollective.com>
2020-11-27 18:58:18 +0000

    Tidy up views

--- views/_layouts/cds.blade.php
+++ views/_layouts/cds.blade.php
@@ -10,17 +10,12 @@
 <body class="page {{ $this->bodyClass }}">
     <div class="page-wrapper">
         <div class="page-content">
-<?php 
-	
+@php
 	[$locationParam] = $this->getParams();
-	
-?>
+@endphp
 	<div class="row-fluid">
 		
-    <?php
-        if (sizeof($this->getLocations()) > 1){
-	?>
-					
+    @if (count($this->getLocations()) > 1)			
 	<div class="list-filter" id="filter-list-filter">
 		
 	    <form id="filter-form" class="form-inline" accept-charset="utf-8" method="GET" action="<?= admin_url('thoughtco/customerdisplay/summary'); ?>" role="form">
@@ -32,9 +27,9 @@
 					<div class="filter-scope date form-group">
 						
 						<select name="location" class="form-control select2-hidden-accessible">
-							<?php 
-								foreach ($this->getLocations() as $key=>$location) echo '<option value="'.$key.'"'.($key == $locationParam ? ' selected' : '').'>'.$location.'</option>'; 
-							?>
+							@foreach ($this->getLocations() as $key=>$location)
+                            <option value="{{ $key }}" @if ($key == $locationParam) selected @endif>{{ $location }}</option>
+                            @endforeach
             			</select>
             			
             		</div>
@@ -42,9 +37,7 @@
 		        </div>	       
 		        		        
 				<div class="col col-1">
-					
-					<button type="submit" class="btn btn-primary float-right"><?= lang('lang:thoughtco.customerdisplay::default.btn_view') ?></button>
-					
+					<button type="submit" class="btn btn-primary float-right">@lang('lang:thoughtco.customerdisplay::default.btn_view')</button>
 				</div>
         
 	    	</div>
@@ -52,22 +45,20 @@
 		</form>
 		
 	</div>
+	@endif 
 	
-    <?php 
-        }
-    ?>	 
     <div class="form-fields">
     	<div class="row">
     		
-    		<?php foreach ($this->renderResults() as $order){ ?>
+			@foreach ($this->renderResults() as $order)
     		 <div class="col col-12">
     			<div class="card w-100 mb-1">
-    				<div class="card-body label-default" style="background-color:<?= $order->status_color; ?>">
-    					<h2 class="card-title mb-1 fa-5x text-center"><?= sprintf(lang('lang:thoughtco.customerdisplay::default.text_'.$order->status), $order->name, $order->id) ?></h2>
+    				<div class="card-body label-default" style="background-color: {{ $order->status_color }}">
+    					<h2 class="card-title mb-1 fa-5x text-center">{!! sprintf(lang('lang:thoughtco.customerdisplay::default.text_'.$order->status), $order->name, $order->id) !!}</h2>
     				</div>
     			</div>	
     		 </div>
-    		 <?php } ?>
+		 	@endforeach
     		 
 	    </div>
     </div>		    	    
